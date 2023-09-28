0d591c4b44d817bf8898b21093057552d9162e01
arif24v <135023226+arif24v@users.noreply.github.com>
2023-09-27 23:01:52 -0400

    Update bnctscript.py
    condensed, accurate variable names

--- bnctscript.py
+++ bnctscript.py
@@ -1,115 +1,59 @@
 import openmc
 import os
 import numpy as np
+import neutronics_material_maker as nmm
 
 # Define materials
-
-# Healthy Skin
-reg_skin_material = openmc.Material()
-reg_skin_material.add_nuclide('B10',3.2e-4)
-reg_skin_material.add_nuclide('C12', 0.116751142398)
-reg_skin_material.add_nuclide('C13', 0.001307857602)
-reg_skin_material.add_nuclide('Ca40', 2.229643e-05)
-reg_skin_material.add_nuclide('Ca42', 1.4881e-07)
-reg_skin_material.add_nuclide('Ca43', 3.105e-08)
-reg_skin_material.add_nuclide('Ca44', 4.7978e-07)
-reg_skin_material.add_nuclide('Ca46', 9.2e-10)
-reg_skin_material.add_nuclide('Ca48', 4.301e-08)
-reg_skin_material.add_nuclide('Cl35', 0.000354578796)
-reg_skin_material.add_nuclide('Cl37', 0.000113421204)
-reg_skin_material.add_nuclide('H1', 0.6198694464951601)
-reg_skin_material.add_nuclide('H2', 9.655350484e-05)
-reg_skin_material.add_nuclide('K39', 0.000125898435)
-reg_skin_material.add_nuclide('K40', 1.5795e-08)
-reg_skin_material.add_nuclide('K41', 9.08577e-06)
-reg_skin_material.add_nuclide('N14', 0.020513582493)
-reg_skin_material.add_nuclide('N15', 7.5417507e-05)
-reg_skin_material.add_nuclide('O16', 0.23977811121)
-reg_skin_material.add_nuclide('O17', 9.109265e-05)
-reg_skin_material.add_nuclide('O18', 0.00048079613999999995)
-reg_skin_material.add_nuclide('P31', 6.6e-05)
-reg_skin_material.add_nuclide('S32', 0.0002927254792)
-reg_skin_material.add_nuclide('S33', 2.3059652e-06)
-reg_skin_material.add_nuclide('S34', 1.29236492e-05)
-reg_skin_material.add_nuclide('S36', 4.49064e-08)
-reg_skin_material.add_nuclide('Zn64', 4.917e-07)
-reg_skin_material.add_nuclide('Zn66', 2.773e-07)
-reg_skin_material.add_nuclide('Zn67', 4.04e-08)
-reg_skin_material.add_nuclide('Zn68', 1.845e-07)
-reg_skin_material.add_nuclide('Zn70', 6.1e-09)
-reg_skin_material.set_density('g/cm3', 1.5)
-
-# Pure Boron-10
-boron_material = openmc.Material()
-boron_material.add_nuclide('B10', 1)
-boron_material.set_density('g/cm3', 7)
-
-# Pure H2O
-water_material = openmc.Material()
-water_material.add_element('H', 2)
-water_material.add_element('O', 1)
-water_material.set_density('g/cm3', 1)
-
 # Tumor Material
-skin_material = openmc.Material()
-skin_material.add_nuclide('H1', 0.106)
-skin_material.add_nuclide('B10', 19.3e-4)
-skin_material.add_nuclide('B11', 85.3e-4)
-skin_material.add_nuclide('C12', 0.14)
-skin_material.add_nuclide('N14', 0.0184)
-skin_material.add_nuclide('O16', 0.726)
-skin_material.add_nuclide('Na22', 0.0014)
-skin_material.add_nuclide('P31', 0.0039)
-skin_material.add_element('Cl', 0.0014)
-skin_material.add_element('K', 0.0039)
-skin_material.set_density('g/cm3',3)
+tumor_material = openmc.Material()
+tumor_material.add_nuclide('H1', 0.106)
+tumor_material.add_nuclide('B10', 19.3e-4)
+tumor_material.add_nuclide('B11', 85.3e-4)
+tumor_material.add_nuclide('C12', 0.14)
+tumor_material.add_nuclide('N14', 0.0184)
+tumor_material.add_nuclide('O16', 0.726)
+tumor_material.add_nuclide('Na22', 0.0014)
+tumor_material.add_nuclide('P31', 0.0039)
+tumor_material.add_element('Cl', 0.0014)
+tumor_material.add_element('K', 0.0039)
+tumor_material.set_density('g/cm3',3)
 
-
+# Regular Skin Material
+skin_material = nmm.Material.from_library(name='Skin (ICRP)', material_id=1)
+skin_material = skin_material.openmc_material
 
 
 # Define materials collection and export to XML
-my_materials = openmc.Materials([boron_material,water_material, skin_material, reg_skin_material])
+my_materials = openmc.Materials([tumor_material, skin_material])
 my_materials.export_to_xml()
 
 # Set cross sections path
-os.environ['OPENMC_CROSS_SECTIONS'] = "/Users/arifv/Desktop/BNCT/lib80x_hdf5/cross_sections.xml"
-openmc.config['cross_sections'] = "/Users/arifv/Desktop/BNCT/lib80x_hdf5/cross_sections.xml"
-openmc.data.cross_sections = "/Users/arifv/Desktop/BNCT/lib80x_hdf5/cross_sections.xml"
-
-# Define cylinders
+os.environ['OPENMC_CROSS_SECTIONS'] = "/Volumes/Untitled/cross_test/endfb-viii.0-hdf5/cross_sections.xml"
 
 
-#Change Tumor Radius, Change Neutron Source Energy, Change B-10 Concentration
-
-# Define radii
-inner_radius = 2
-middle_radius = 5
-outer_radius=12
+# Change Tumor Radius, Change Neutron Source Energy, Change B-10 Concentration
+# Define radii in cm
+inner_radius = 0.4
+middle_radius = 0.5
 
 # Transmission boundary (default) allows neutrons to cross
 inner_cylinder = openmc.ZCylinder(r=inner_radius, boundary_type='transmission')
-middle_cylinder = openmc.ZCylinder(r=middle_radius, boundary_type='transmission')
-outer_cylinder = openmc.ZCylinder(r=outer_radius, boundary_type='vacuum')
-#top_plane = openmc.ZPlane(z0=10.0, boundary_type='reflective')
-#bottom_plane = openmc.ZPlane(z0=-10.0, boundary_type='reflective')
+middle_cylinder = openmc.ZCylinder(r=middle_radius, boundary_type='vacuum')
+top_plane = openmc.ZPlane(z0=10.0, boundary_type='vacuum') #10 cm high
+bottom_plane = openmc.ZPlane(z0=-10.0, boundary_type='vacuum')
 
 # Define cells
-inner_slab = openmc.Cell(region=-inner_cylinder, fill=reg_skin_material)
-middle_slab = openmc.Cell(region=+inner_cylinder & -middle_cylinder, fill=skin_material)
-outer_slab = openmc.Cell(region=+middle_cylinder & -outer_cylinder, fill=water_material)
+inner_slab = openmc.Cell(region=-inner_cylinder, fill=skin_material)
+middle_slab = openmc.Cell(region=+inner_cylinder & -middle_cylinder, fill=tumor_material)
 
 # Define geometry
-geometry = openmc.Geometry([inner_slab, middle_slab, outer_slab])
-
-# Export geometry to XML
+geometry = openmc.Geometry([inner_slab, middle_slab])
 geometry.export_to_xml()
 
 # Define source
 point = openmc.stats.Point((0, 0, 0))
-#energy_distribution = openmc.stats.Discrete([1e6], [1])
-lower = 1
-upper = 1e3
-energy_distribution = openmc.stats.PowerLaw(1, 1e3, -1)
+
+energy_distribution = openmc.stats.PowerLaw(1, 1000, -1.001)
 source = openmc.Source(space=point, energy=energy_distribution)
 
 # Define settings
@@ -120,109 +64,69 @@ settings.particles = 10000
 settings.run_mode = 'fixed source'
 settings.source = source
 
+# https://www.w3resource.com/numpy/array-creation/logspace.php
+# neutrons from 10^-5 to 10^10 eV with 50 equally space bins
+energy_bins = np.logspace(-5, 4, 50)
+
 # Define filters
 neutron_particle_filter = openmc.ParticleFilter(['neutron'])
 inner_slab_filter = openmc.CellFilter(inner_slab)
 middle_slab_filter = openmc.CellFilter(middle_slab)
 
-#energy_bins = np.logspace(np.log10(lower*0.001), np.log10(upper*0.001), 50)
-
-# as you increase the bins, the sum of all of them approaches the true reaction rate, 
-# can express this as an integral to find total reaction rate over different neutron energies
-# increase upper to get more accurate result
-# https://www.w3resource.com/numpy/array-creation/logspace.php
-# neutrons from 10^-5 to 10^10 eV with 50 equally space bins
-energy_bins = np.logspace(-5, 4, 50)
-
-
-# Define tallies
-
-# Reaction Rates (not important)
-boron_tally = openmc.Tally(1)
-boron_tally.scores = ['(n,a)']
-boron_tally.filters = [neutron_particle_filter, inner_slab_filter, openmc.EnergyFilter(energy_bins)]
-
-
-nitrogen_tally = openmc.Tally(2)
-nitrogen_tally.scores = ['(n,a)']
-nitrogen_tally.filters = [neutron_particle_filter, middle_slab_filter]
-
-# Calculate dose using dose coefficients
+# Define Dose Coefficients
 energy_bins_n, dose_coeffs_n = openmc.data.dose_coefficients(particle="neutron", geometry="AP")
 energy_function_filter_n = openmc.EnergyFunctionFilter(energy_bins_n, dose_coeffs_n)
 energy_function_filter_n.interpolation = "cubic"  # cubic interpolation is recommended by ICRP
 
+# Define tallies
+# Calculate dose using dose coefficients
+inner_flux = openmc.Tally(name="inner_flux")
+inner_flux.filters = [inner_slab_filter, neutron_particle_filter]
+inner_flux.scores=["flux"]
 
-flux_inner_tally = openmc.Tally(name="flux_inner_tally")
-flux_inner_tally.filters = [inner_slab_filter, neutron_particle_filter]
-flux_inner_tally.scores=["flux"]
+middle_flux = openmc.Tally(name="middle_flux")
+middle_flux.filters = [middle_slab_filter, neutron_particle_filter]
+middle_flux.scores=["flux"]
 
-flux_middle_tally = openmc.Tally(name="flux_middle_tally")
-flux_middle_tally.filters = [middle_slab_filter, neutron_particle_filter]
-flux_middle_tally.scores=["flux"]
+tumor_na = openmc.Tally(name="tumor_na")
+tumor_na.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n, 
+                    openmc.EnergyFilter(energy_bins)]
+tumor_na.scores = ["(n,a)"]
 
-# (n,a) tally for tumor region
-dose_cell_tally_na = openmc.Tally(name="neutron_na_dose_on_cell")
-dose_cell_tally_na.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n]
-dose_cell_tally_na.scores = ["(n,a)"]
+tumor_ga = openmc.Tally(name="tumor_ga")
+tumor_ga.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n, 
+                    openmc.EnergyFilter(energy_bins)]
+tumor_ga.scores = ["(n,gamma)"]
 
-# (n,gamma) tally for tumor region
-dose_cell_tally_ga = openmc.Tally(name="neutron_ga_dose_on_cell")
-dose_cell_tally_ga.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n]
-dose_cell_tally_ga.scores = ["(n,gamma)"]
+check_tumor_ga = openmc.Tally(name="check_tumor_ga")
+check_tumor_ga.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n]
+check_tumor_ga.scores = ["(n,gamma)"]
 
-# (n,p) tally for tumor region
-dose_cell_tally_p = openmc.Tally(name="neutron_p_dose_on_cell")
-dose_cell_tally_p.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n]
-dose_cell_tally_p.scores = ["(n,p)"]
+tumor_np = openmc.Tally(name="tumor_np")
+tumor_np.filters = [middle_slab_filter, neutron_particle_filter, energy_function_filter_n, 
+                             openmc.EnergyFilter(energy_bins)]
+tumor_np.scores = ["(n,p)"]
 
+skin_na = openmc.Tally(name="skin_na")
+skin_na.filters = [inner_slab_filter, neutron_particle_filter, energy_function_filter_n,
+                   openmc.EnergyFilter(energy_bins)]
+skin_na.scores = ["(n,a)"]
 
-# (n,a) tally for healthy skin region
-reg_dose_cell_tally_na = openmc.Tally(name="reg_neutron_na_dose_on_cell")
-reg_dose_cell_tally_na.filters = [inner_slab_filter, neutron_particle_filter, energy_function_filter_n]
-reg_dose_cell_tally_na.scores = ["(n,a)"]
-
-# (n,gamma) tally for healthy skin region
-reg_dose_cell_tally_ga = openmc.Tally(name="reg_neutron_ga_dose_on_cell")
-# Include the EnergyFunctionFilter as a filter
-reg_dose_cell_tally_ga.filters = [inner_slab_filter, neutron_particle_filter, energy_function_filter_n]
-reg_dose_cell_tally_ga.scores = ["(n,gamma)"]
-
-# (n,p) for healthy skin region
-reg_dose_cell_tally_p = openmc.Tally(name="reg_neutron_p_dose_on_cell")
-# Include the EnergyFunctionFilter as a filter
-reg_dose_cell_tally_p.filters = [inner_slab_filter, neutron_particle_filter, energy_function_filter_n]
-reg_dose_cell_tally_p.scores = ["(n,p)"]
+skin_ga = openmc.Tally(name="skin_ga")
+skin_ga.filters = [inner_slab_filter, neutron_particle_filter, energy_function_filter_n,
+                   openmc.EnergyFilter(energy_bins)]
+skin_ga.scores = ["(n,gamma)"]
 
 
-#cylindrical_mesh = openmc.CylindricalMesh.from_domain(
-#    domain=geometry, # the corners of the mesh are being set automatically to surround the geometry
-#    dimension=[1, 1, 1] # 100 voxels in each axis direction (r, z, phi)
-#)
-
-#mesh_filter = openmc.MeshFilter(cylindrical_mesh)
-#cyl_tally = openmc.Tally()
-#cyl_tally.scores = ['(n,a)']
-#cyl_tally.filters = [mesh_filter, neutron_particle_filter, energy_function_filter_n]
-
-#cyl_tally1 = openmc.Tally()
-#cyl_tally1.scores = ['(n,p)']
-#cyl_tally1.filters = [mesh_filter, neutron_particle_filter, energy_function_filter_n]
-
-#cyl_tally2 = openmc.Tally()
-#cyl_tally2.scores = ['(n,gamma)']
-#cyl_tally2.filters = [mesh_filter, neutron_particle_filter, energy_function_filter_n]
+skin_np = openmc.Tally(name="skin_np")
+skin_np.filters = [inner_slab_filter, neutron_particle_filter, energy_function_filter_n,
+                   openmc.EnergyFilter(energy_bins)]
+skin_np.scores = ["(n,p)"]
 
 
-my_tallies = openmc.Tallies([boron_tally, nitrogen_tally, dose_cell_tally_na, 
-                             dose_cell_tally_ga, dose_cell_tally_p, reg_dose_cell_tally_na, 
-                             reg_dose_cell_tally_ga, reg_dose_cell_tally_p, flux_middle_tally, flux_inner_tally])
-
-
+my_tallies = openmc.Tallies([inner_flux,middle_flux,tumor_na,tumor_np,tumor_ga,skin_na,skin_np,
+                             skin_ga,check_tumor_ga])
 settings.tallies = my_tallies
-
-
-
 
 # Create the model and run simulation
 model = openmc.model.Model(geometry, my_materials, settings, my_tallies)
@@ -233,97 +137,68 @@ model.run(openmc_exec=openmc_exec)
 
 statepoint = openmc.StatePoint('statepoint.10.h5')
 
-boron_tally_result = statepoint.tallies[boron_tally.id].mean
-nitrogen_tally_result = statepoint.tallies[nitrogen_tally.id].mean
-middle_flux = statepoint.tallies[flux_middle_tally.id].mean
-inner_flux = statepoint.tallies[flux_inner_tally.id].mean
 
-# Print out the (n,a) reaction rate
-print("Inner total reaction rate:", np.sum(boron_tally_result))
-print("(n,a) reaction rate:", nitrogen_tally_result)
-print("middle flux:", middle_flux)
-print("inner flux:", inner_flux)
-
+inner_flux = statepoint.tallies[inner_flux.id]
+middle_flux = statepoint.tallies[middle_flux.id]
+print("Inner Flux Before .mean method:",inner_flux)
+print("Middle Flux Before .mean method:",middle_flux)
+inner_flux = statepoint.tallies[inner_flux.id].mean
+middle_flux = statepoint.tallies[middle_flux.id].mean
+print("Inner Flux After .mean method:",inner_flux)
+print("Middle Flux After .mean method:", middle_flux)
 
 # Define RBE values for various interactions
-
-doseDict = {1: {'name': 'neutron_na_dose_on_cell', 'conversion': 20,'RBE':3.8}, #n,a
-            2: {'name': 'neutron_ga_dose_on_cell', 'conversion': 1, 'RBE':1},   #n,gamma
-            3: {'name': 'neutron_p_dose_on_cell', 'conversion': 1, 'RBE': 3.2}    #n,p
+tumor_doseDict = {1: {'name': 'tumor_na', 'conversion': 20,'RBE':3.8}, #n,a
+            2: {'name': 'tumor_ga', 'conversion': 1, 'RBE':1},   #n,gamma
+            3: {'name': 'tumor_np', 'conversion': 1, 'RBE': 3.2}    #n,p
            }
 
-reg_doseDict = {1: {'name': 'reg_neutron_na_dose_on_cell', 'conversion': 20,'RBE':1.3}, #n,a
-                2: {'name': 'reg_neutron_ga_dose_on_cell', 'conversion': 1, 'RBE':1},   #n,gamma
-                3: {'name': 'reg_neutron_p_dose_on_cell', 'conversion': 1, 'RBE': 3.2}    #n,p
+doseDict_check = {1: {'name': 'neutron_na_s_dose_on_cell', 'conversion': 20,'RBE':3.8}, #n,a
+            2: {'name': 'neutron_ga_s_dose_on_cell', 'conversion': 1, 'RBE':1},   #n,gamma
+            3: {'name': 'neutron_p_s_dose_on_cell', 'conversion': 1, 'RBE': 3.2}    #n,p
+           }
+
+skin_doseDict = {1: {'name': 'skin_na', 'conversion': 20,'RBE':1.3}, #n,a
+                2: {'name': 'skin_ga', 'conversion': 1, 'RBE':1},   #n,gamma
+                3: {'name': 'skin_np', 'conversion': 1, 'RBE': 3.2}    #n,p
                }
 
 D_TOT = 0
 reg_D_TOT = 0
 neutrons_per_second = 1e12 # Subject to change
 
+
+tumor_na_results = statepoint.get_tally(name='tumor_na').mean.flatten()
+tumor_ga_results = statepoint.get_tally(name='tumor_ga').mean.flatten()
+tumor_np_results = statepoint.get_tally(name='tumor_np').mean.flatten()
+
+skin_na_results = statepoint.get_tally(name='skin_na').mean.flatten()
+skin_ga_results = statepoint.get_tally(name='skin_ga').mean.flatten()
+skin_np_results = statepoint.get_tally(name='skin_np').mean.flatten()
+
+
+tumor_dose = 0
+skin_dose = 0
+
 for i in range (1,4):
-
-    # Use the StatePoint data to get the tally result, pSv-cm^3/source neutron
-    #figure out what mean.flatten[0] does
-    print(statepoint.get_tally(name=doseDict[i]['name']).mean)
-    tally_result = statepoint.get_tally(name=doseDict[i]['name']).mean.flatten()[0]
-    print(tally_result)
-
-    #pSv-cm^3/second
-    tally_result = tally_result * middle_flux * neutrons_per_second
-
-    middle_slab_volume_cm3 = np.pi*(middle_radius**2 - inner_radius**2)
-
-    #psv/second
-    tally_result = tally_result/middle_slab_volume_cm3
-
-    #60 seconds in a minute, 60 minutes in an hour, x hours
-    #pSv/hour
-    tally_dose_pSv = tally_result * 60 * 60
-
-    tally_dose_Sv = tally_dose_pSv * 1e-12
-
-    tally_dose_Gy = tally_dose_Sv/doseDict[i]['conversion']
-    
-    #Gy/hour alpha particles
-    print("Total dose in Gy/hr:", tally_dose_Gy,doseDict[i]['name'])
-    
-    D_TOT= D_TOT + tally_dose_Gy * doseDict[i]['RBE']
+    for j, tally_result in enumerate(statepoint.get_tally(name=tumor_doseDict[i]['name']).mean.flatten()):
+        tally_result = tally_result * middle_flux.flatten() * neutrons_per_second
+        middle_slab_volume_cm3 = np.pi*(middle_radius**2 - inner_radius**2)
+        tally_result = tally_result/middle_slab_volume_cm3
+        tally_dose_pSv = tally_result * 60 * 60
+        tally_dose_Sv = tally_dose_pSv * (1e-12)
+        tally_dose_Gy = tally_dose_Sv/tumor_doseDict[i]['conversion']
+        tumor_dose= tumor_dose + tally_dose_Gy * tumor_doseDict[i]['RBE']
+    print(i,"th",tumor_dose)
 for i in range (1,4):
-
-    tally_result = statepoint.get_tally(name=reg_doseDict[i]['name']).mean.flatten()[0]
-
-    #pSv-cm^3/second
-    tally_result = tally_result * inner_flux * neutrons_per_second
-
-    inner_slab_volume_cm3 = np.pi*(inner_radius**2)
-
-    #psv/second
-    tally_result = tally_result/inner_slab_volume_cm3
-
-    #60 seconds in a minute, 60 minutes in an hour
-    #pSv/hour
-    tally_dose_pSv = tally_result * 60 * 60
-
-    tally_dose_Sv = tally_dose_pSv * 1e-12
-
-    tally_dose_Gy = tally_dose_Sv/reg_doseDict[i]['conversion']
-    
-    #Gy/hour alpha particles
-    print("Total dose in Gy/hr:", tally_dose_Gy, reg_doseDict[i]['name'])
-    
-    reg_D_TOT= reg_D_TOT + tally_dose_Gy * reg_doseDict[i]['RBE']
-print ("DTOT/hr:", D_TOT)
-print ("reg_DTOT/hr:", reg_D_TOT)
-
-
-boron_tally_results = statepoint.tallies[boron_tally.id].mean
-energy_integrated_result = np.sum(boron_tally_results)  # Sum over all energy bins
-
-# Print the energy-integrated result
-print("Energy-Integrated Boron Tally Result:", energy_integrated_result)
-
-# Access results within specific energy bins
-for i, energy_bin_result in enumerate(boron_tally_results):
-    print(f"Energy Bin {i}: {energy_bins[i]} to {energy_bins[i+1]} eV")
-    print("Tally Result:", energy_bin_result)
+    for j, tally_result in enumerate(statepoint.get_tally(name=skin_doseDict[i]['name']).mean.flatten()):
+        tally_result = tally_result * inner_flux.flatten() * neutrons_per_second
+        inner_slab_volume_cm3 = np.pi*(inner_radius**2)
+        tally_result = tally_result/inner_slab_volume_cm3
+        tally_dose_pSv = tally_result * 60 * 60
+        tally_dose_Sv = tally_dose_pSv * (1e-12)
+        tally_dose_Gy = tally_dose_Sv/skin_doseDict[i]['conversion']
+        skin_dose = skin_dose + tally_dose_Gy * skin_doseDict[i]['RBE']
+    print(i,"th",skin_dose)  
+print ("DTOT/hr:", tumor_dose)
+print ("reg_DTOT/hr:", skin_dose)
